26ab00cf83c572d01d4c1e74820210c816a8b173
Mavlyanov <kadyrbek.mavlyanov@gmail.com>
2020-09-13 12:46:03 +0600

    initial commit

---
+++ test/src/main/java/km/ukuk/test/TestApplication.java
@@ -0,0 +1,13 @@
+package km.ukuk.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+}
